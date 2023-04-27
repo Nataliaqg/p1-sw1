10f0086148959d798bc48640a14db804ce7b167a
Nataliaqg <nataliaquirogag@gmail.com>
2023-04-27 11:00:26 -0400

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PhotoFiesta</title>
 </head>
+
 <body>
     <h1>Esta es la vista Saas</h1>
-    @guest
         @if (Route::has('login'))
             <a href="{{ route('login') }}">login</a>
         @endif
@@ -16,6 +17,6 @@
         @if (Route::has('register'))
             <a href="{{ route('register') }}">register</a>
         @endif
-    @endguest
 </body>
+
 </html>
