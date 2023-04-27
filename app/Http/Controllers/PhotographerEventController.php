bda4a59749b36704548bc35068b7fc828c592bca
jsoliz064 <jsoliz064@gmail.com>
2023-04-27 11:46:47 -0400

    add view cruds/photographer-event and component livewire/photographer-event/index

--- app/Http/Controllers/PhotographerEventController.php
+++ app/Http/Controllers/PhotographerEventController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PhotographerEventController extends Controller
 {
-    //
+    public function index(){
+        return view('cruds.photographer-event');
+    }
 }
---
+++ resources/views/livewire/photographer-event/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>componente livewire (aqui  hacer la vista para cargar imagenes)</h1>
+</div>
