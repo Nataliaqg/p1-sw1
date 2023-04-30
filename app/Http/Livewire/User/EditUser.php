5ea5b358a6b7289be44c43f85d884afba693df61
jsoliz064 <jsoliz064@gmail.com>
2023-04-30 18:11:34 -0400

    upload img user

--- app/Http/Livewire/User/EditUser.php
+++ app/Http/Livewire/User/EditUser.php
@@ -4,15 +4,69 @@ namespace App\Http\Livewire\User;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class EditUser extends Component
-{   
-    public $user=[];
+{
+    use WithFileUploads;
+    public $user = [];
+    public $photo_path1, $photo_path2, $photo_path3;
+
+    public function __construct()
+    {
+        $user = Auth()->user();
+        $this->user = User::find($user->id)->toArray();
+    }
+
     public function render()
     {
-        $user= Auth()->user();
-        $this->user= User::find($user->id)->toArray();
         //dd($user);
-        return view('livewire.user.edit-user',compact('user'));
+        return view('livewire.user.edit-user');
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'user.name' => 'required',
+            'user.birthday' => 'required',
+        ]);
+        $user = User::find($this->user['id']);
+
+        if ($this->photo_path1 !== null) {
+            if ($user->photo_path1) {
+                $ruta = "public" . $user->photo_path1;
+                if (file_exists("../" . $ruta)) {
+                    unlink("../" . $ruta);
+                }
+            }
+            $imagenes = $this->photo_path1->store('documents', 'public');
+            $user->photo_path1 = Storage::url($imagenes);
+        }
+        if ($this->photo_path2 !== null) {
+            if ($user->photo_path2) {
+                $ruta = "public" . $user->photo_path2;
+                if (file_exists("../" . $ruta)) {
+                    unlink("../" . $ruta);
+                }
+            }
+            $imagenes = $this->photo_path2->store('documents', 'public');
+            $user->photo_path2 = Storage::url($imagenes);
+        }
+        if ($this->photo_path3 !== null) {
+            if ($user->photo_path3) {
+                $ruta = "public" . $user->photo_path3;
+                if (file_exists("../" . $ruta)) {
+                    unlink("../" . $ruta);
+                }
+            }
+            $imagenes = $this->photo_path3->store('documents', 'public');
+            $user->photo_path3 = Storage::url($imagenes);
+        }
+
+        $user->name=$this->user['name'];
+        $user->birthday=$this->user['birthday'];
+        $user->save();
+        $this->user=$user->toArray();
     }
 }
--- resources/views/livewire/user/edit-user.blade.php
+++ resources/views/livewire/user/edit-user.blade.php
@@ -15,36 +15,59 @@
             </div>
         </div>
     </header>
-    <div class="container-xl px-4 mt-4">
+    <div class="container-xl px-4 mt-4" >
         <div class="row">
             <div class="col-xl-4">
                 {{-- CARRUSEL --}}
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                            class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
-                      <div class="carousel-item active" >
-                        <img src="{{asset('assets/img/profile-example/Camila1.jpg')}}" class="d-block w-100" style=" max-height: 520px;" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="{{asset('assets/img/profile-example/Camila2.jpg')}}" class="d-block w-100" style=" max-height: 520px;" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="{{asset('assets/img/profile-example/camila3.jpg')}}" class="d-block w-100"  style=" max-height: 520px;" alt="...">
-                      </div>
+                        <div class="carousel-item active">
+                            <img src="{{ asset($this->user['photo_path1']) }}" class="d-block w-100"
+                                style=" max-height: 520px;" alt="...">
+                            <label style="z-index: 99;" for="photo_path1"
+                                class="btn btn-edit btn-dark position-absolute top-0 start-0"><i
+                                    class="fas fa-pencil-alt"></i></label>
+                            <input wire:model='photo_path1' accept="image/*" type="file" id="photo_path1"
+                                style="display:none">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset($this->user['photo_path2']) }}" class="d-block w-100"
+                                style=" max-height: 520px;" alt="...">
+                            <label style="z-index: 99;" for="photo_path2"
+                                class="btn btn-edit btn-dark position-absolute top-0 start-0"><i
+                                    class="fas fa-pencil-alt"></i></label>
+                            <input wire:model.lazy='photo_path2' accept="image/*" type="file" id="photo_path2"
+                                style="display:none">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset($this->user['photo_path3']) }}" class="d-block w-100"
+                                style=" max-height: 520px;" alt="...">
+                            <label style="z-index: 99;" for="photo_path3"
+                                class="btn btn-edit btn-dark position-absolute top-0 start-0"><i
+                                    class="fas fa-pencil-alt"></i></label>
+                            <input wire:model.lazy='photo_path3' accept="image/*" type="file" id="photo_path3"
+                                style="display:none">
+                        </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
                     </button>
-                  </div>
+                </div>
             </div>
             <div class="col-xl-8">
                 <!-- Account details card-->
@@ -54,7 +77,6 @@
                         <form>
                             <form method="POST" action="{{ route('register') }}">
                                 @csrf
-
                                 {{-- Nombre --}}
                                 <div class="row mb-3">
                                     <label for="name"
@@ -62,10 +84,10 @@
 
                                     <div class="col-md-6">
                                         <input id="name" type="text"
-                                            class="form-control @error('name') is-invalid @enderror" name="name"
-                                            required autocomplete="name" autofocus wire:model='user.name'>
-                                            
-                                        @error('name')
+                                            class="form-control @error('user.name') is-invalid @enderror"
+                                            wire:model.lazy='user.name'>
+
+                                        @error('user.name')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
@@ -80,9 +102,9 @@
                                     <div class="col-md-6">
                                         <input id="dni" type="text"
                                             class="form-control @error('dni') is-invalid @enderror" name="dni"
-                                            required autocomplete="dni" wire:model='user.dni'>
+                                            required autocomplete="dni" wire:model.lazy='user.dni'>
 
-                                        @error('dni')
+                                        @error('user.dni')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
@@ -97,7 +119,7 @@
                                     <div class="col-md-6">
                                         <input id="phone" type="tel"
                                             class="form-control @error('phone') is-invalid @enderror" name="phone"
-                                            required autocomplete="phone" wire:model='user.phone'>
+                                            required autocomplete="phone" wire:model.lazy='user.phone'>
 
                                         @error('phone')
                                             <span class="invalid-feedback" role="alert">
@@ -114,7 +136,7 @@
                                     <div class="col-md-6">
                                         <input id="address" type="text"
                                             class="form-control @error('address') is-invalid @enderror" name="address"
-                                            required autocomplete="address" wire:model='user.address'>
+                                            required autocomplete="address" wire:model.lazy='user.address'>
 
                                         @error('address')
                                             <span class="invalid-feedback" role="alert">
@@ -131,7 +153,8 @@
                                     <div class="col-md-6">
                                         <input id="birthday" type="date"
                                             class="form-control @error('birthday') is-invalid @enderror"
-                                            name="birthday" required autocomplete="birthday" wire:model='user.birthday'>
+                                            name="birthday" required autocomplete="birthday"
+                                            wire:model.lazy='user.birthday'>
 
                                         @error('birthday')
                                             <span class="invalid-feedback" role="alert">
@@ -148,7 +171,8 @@
                                     <div class="col-md-6">
                                         <input id="email" type="email"
                                             class="form-control @error('email') is-invalid @enderror" name="email"
-                                            value="{{ old('email') }}" required autocomplete="email" wire:model='user.email'>
+                                            value="{{ old('email') }}" required autocomplete="email"
+                                            wire:model.lazy='user.email'>
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -159,13 +183,17 @@
                                 </div>
                             </form>
                             <!-- Save changes button-->
-                            <button class="btn btn-primary" type="button">Guardar cambios</button>
+                            <button class="btn btn-primary" wire:click='update()' ype="button">Guardar
+                                cambios</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <link href="{{ asset('svg/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap/bootstrap.bundle.min.js') }}"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             // Evento para la foto 1
